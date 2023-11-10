0dcf4bfd313f62eb6d33a72ca15064fa6fd3ddb7
shokhrukh rakhmatov <feruzjalilov7314@gmail.com>
2023-11-10 19:07:52 +0500

    audit add

--- src/hrPages/Audit/index.js
+++ src/hrPages/Audit/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import SearchTop from "../../components/SearchTop";
 import Paper from "@mui/material/Paper";
 import Table from "@mui/material/Table";
@@ -10,19 +10,20 @@ import TableContainer from "@mui/material/TableContainer";
 import Stack from "@mui/material/Stack";
 import Pagination from "@mui/material/Pagination";
 import AuditSearchTop from "./AuditSearchTop";
+import axios from "axios";
+import {API_PATH, CONFIG} from "../../components/const";
 
 const Audit = () => {
-    function createData(name, calories, fat, carbs, protein) {
-        return {name, calories, fat, carbs, protein};
+    const [data, setData] = useState([])
+    const getList = () => {
+         axios.get(API_PATH + "company/" + localStorage.getItem('id') + "/audit/all", CONFIG)
+            .then(res =>{
+                setData(res.data)
+            })
     }
-
-    const rows = [
-        createData('Ali Muzaffarovt', 159, 6.0, 24, 4.0),
-        createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-        createData('Eclair', 262, 16.0, 24, 6.0),
-        createData('Cupcake', 305, 3.7, 67, 4.3),
-        createData('Gingerbread', 356, 16.0, 49, 3.9),
-    ];
+    useEffect(() => {
+        getList()
+    }, []);
     return (
         <div className="audit">
             <div className="audit-header">
@@ -48,18 +49,19 @@ const Audit = () => {
                             </TableRow>
                         </TableHead>
                         <TableBody>
-                            {rows.map((row) => (
+                            {
+                                data?.slice(0, 20).map((item) => (
                                 <TableRow
-                                    key={row.name}
+                                    key={item.name}
                                     sx={{'&:last-child td, &:last-child th': {border: 0}}}
                                 >
                                     <TableCell component="th" scope="row">
-                                        {row.name}
+                                        {item?.employee?.first_name + "  "  + item?.employee?.last_name}
                                     </TableCell>
-                                    <TableCell className="twt" align="right">{row.calories}</TableCell>
-                                    <TableCell className="twt" align="right">{row.calories}</TableCell>
+                                    <TableCell className="twt" align="right">none</TableCell>
+                                    <TableCell className="twt" align="right">{item?.created_at.slice(11, 16)}</TableCell>
                                     <TableCell className="twt" align="right">
-                                        <span className="font-family-medium">Добавил нового сотрудника</span>
+                                        <span className="font-family-medium">{item?.description}</span>
                                     </TableCell>
                                 </TableRow>
                             ))}
--- src/hrPages/Home/index.js
+++ src/hrPages/Home/index.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import {Select} from 'antd';
+import {DatePicker, Select} from 'antd';
 import Radio from '@mui/material/Radio';
 import RadioGroup from '@mui/material/RadioGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
@@ -12,59 +12,74 @@ import {toast} from "react-toastify";
 import {PieChart} from "@mui/x-charts/PieChart";
 import {styled} from "@mui/material/styles";
 import {useDrawingArea} from "@mui/x-charts/hooks";
+import dayjs from "dayjs";
 
-const data = [
-    { value: 5, label: 'Отсутствие' },
-    { value: 10, label: 'Опоздание' },
-    { value: 15, label: 'Вовремя' },
-];
+const {RangePicker} = DatePicker;
+
 
 const size = {
     width: 400,
     height: 200,
 };
 
-
-const StyledText = styled('text')(({ theme }) => ({
+const StyledText = styled('text')(({theme}) => ({
     fill: theme.palette.text.primary,
     textAnchor: 'middle',
     dominantBaseline: 'central',
     fontSize: 20,
 }));
 
-function PieCenterLabel({ children }) {
-    const { width, height, left, top } = useDrawingArea();
+function PieCenterLabel({children}) {
+    const {width, height, left, top} = useDrawingArea();
     return (
         <StyledText x={left + width / 2} y={top + height / 2}>
             {children}
         </StyledText>
     );
 }
+
 const Home = () => {
     const [time, setTime] = useState(new Date());
     const [tabs, setTabs] = useState(1)
     const [age, setAge] = React.useState('');
-    const [data, setData] = useState([])
+    const [data2, setData2] = useState([])
     const [yesterday, setYesterday] = useState(getPreviousDay())
     const [absent_attendances_count, setabsent_attendances_count] = useState(0)
     const [late_attendances_count, setlate_attendances_count] = useState(0)
     const [on_time_attendances_count, seton_time_attendances_count] = useState(0)
     const [topType, settopType] = useState(1)
+    const dateFormat = 'YYYY-MM-DD';
+    const [offices, setOffices] = useState([])
+    const [building_id, setbuilding_id] = useState(1)
+    const [departments, setDepartments] = useState([])
+    const [department_id, setdepartment_id] = useState(1)
 
-        const handleChange = (event) => {
-        setAge(event.target.value);
-    };
+
+    const [data, setData] = useState([]);
+
     const getAll = (e) => {
         axios.get(API_PATH + "company/" + localStorage.getItem('id') + "/hr/attendance/all/?status=" + e + "&for_date=" + +time?.getFullYear() + "-" + Number(time?.getMonth() + 1) + "-" + Number(time?.getDate()), CONFIG)
             .then(res => {
-                setData(res.data)
+                setData2(res.data)
             })
     }
+
+
     function getPreviousDay(date = new Date()) {
         const previous = new Date(date.getTime());
         previous.getDay(date.getDay() - 1);
 
         return previous;
+    }
+
+    const getBuilding = () => {
+        axios.get(API_PATH + "company/" + localStorage.getItem('id') + "/building/all", CONFIG)
+            .then(res => {
+                setOffices(res.data)
+            })
+            .catch(err => {
+                toast.error("Ошибка")
+            })
     }
     const today = new Date() // get today's date
     const tomorrow = new Date(today)
@@ -76,31 +91,61 @@ const Home = () => {
     //             setData(res.data)
     //         })
     // }
-    const getStatistik1 = (to_date) => {
-        axios.get(API_PATH + "company/" + localStorage.getItem('id') + "/hr/attendance/statistics/?from_date=" +time?.getFullYear() + "-" + Number(time?.getMonth() + 1) + "-" + Number(time?.getDate()) + "&to_date="+time?.getFullYear() + "-" + Number(time?.getMonth() + 1) + "-" + Number(to_date) + "&building_id=1&department_id=1" + Number(time?.getDate()), CONFIG)
+    const getStatistik1 = (from_date, to_date) => {
+        axios.get(API_PATH + "company/" + localStorage.getItem('id') + "/hr/attendance/statistics/?from_date=" + from_date + "&to_date=" + to_date + "&building_id=" + building_id + "&department_id=" + department_id + Number(time?.getDate()), CONFIG)
+            .then(res => {
+                // setData(res?.data)
+                setabsent_attendances_count(res?.data?.absent_attendances_count)
+                seton_time_attendances_count(res?.data?.on_time_attendances_count)
+                setlate_attendances_count(res?.data?.late_attendances_count)
+                setData([{
+                    value: res?.data?.absent_attendances_count,
+                    label: 'Отсутствие'
+                }, {
+                    value: res?.data?.on_time_attendances_count,
+                    label: 'Вовремя'
+                }, {value: res?.data?.late_attendances_count, label: 'Опоздание'}])
+
+            })
+            .catch(err => {
+                toast.error("No data")
+            })
+    }
+
+    function onChange(date, dateString) {
+        console.log(date)
+        console.log(dateString[0])
+        console.log(dateString[1])
+        axios.get(API_PATH + "company/" + localStorage.getItem('id') + "/hr/attendance/statistics/?from_date=" + dateString[0] + "&to_date=" + dateString[1]+ "&building_id=" + building_id + "&department_id=" + department_id + Number(time?.getDate()), CONFIG)
             .then(res => {
                 // setData(res?.data)
                 setabsent_attendances_count(res?.data?.absent_attendances_count)
                 seton_time_attendances_count(res?.data?.on_time_attendances_count)
                 setlate_attendances_count(res?.data?.late_attendances_count)
             })
-            .catch(err =>{
+            .catch(err => {
                 toast.error("No data")
             })
     }
-    function calculate(fr, sc, th) {
-        return console.log(fr + sc + th)
+
+    const getDeps = () => {
+        axios.get(API_PATH + "company/" + localStorage.getItem('id') + "/hr/department/all", CONFIG)
+            .then(res => {
+                setDepartments(res.data)
+            })
+            .catch(err => {
+                toast.error("Ошибка")
+            })
     }
+
     useEffect(() => {
         getAll('on_time')
+        getDeps()
+        getBuilding()
         // getStatistik()
-        getStatistik1(tomorrow.toDateString().slice(8, 10))
-        console.log(tomorrow.toDateString().slice(8, 10))
-        console.log(yesterday)
-        console.log(yesterday)
-        console.log(yesterday)
-        console.log()
-    }, []);
+        getStatistik1(Number(time?.getFullYear()) + "-" + Number(time?.getMonth() + 1) + "-" + Number(time?.getDate()), time?.getFullYear() + "-" + Number(time?.getMonth() + 1) + "-" + Number(tomorrow.toDateString().slice(8, 10)))
+
+    }, [building_id]);
 
     return (
         <div className="home-style">
@@ -134,41 +179,35 @@ const Home = () => {
                             <label className="font-family-medium">Филиалы</label>
                             <Select
                                 className="w-100"
-                                onChange={handleChange}
-                                options={[
-                                    {value: 'jack', label: 'Jack'},
-                                    {value: '1', label: '1'},
-                                    {value: 'Yiminghe', label: 'yiminghe'},
-                                    {value: 'Yiminghe', label: 'yiminghe'},
-                                    {value: 'Yiminghe', label: 'yiminghe'},
-                                    {value: 'Yiminghe', label: 'yiminghe'},
-                                    {value: 'Yiminghe', label: 'yiminghe'},
-                                    {value: 'disabled', label: 'Disabled', disabled: true},
-                                ]}
-                            />
+                                value={building_id}
+                                onChange={(e) => setbuilding_id(e)}
+                            >
+                                {
+                                    offices?.map((item, index) => (
+                                        <option value={item?.id} key={index}>{item?.name}</option>
+                                    ))
+                                }
+                            </Select>
                         </div>
                         <div className="center-sv">
                             <label className="font-family-medium">Отделы</label>
                             <Select
                                 className="w-100"
-                                onChange={handleChange}
-                                options={[
-                                    {value: 'jack', label: 'Jack'},
-                                    {value: '1', label: '1'},
-                                    {value: 'Yiminghe', label: 'yiminghe'},
-                                    {value: 'Yiminghe', label: 'yiminghe'},
-                                    {value: 'Yiminghe', label: 'yiminghe'},
-                                    {value: 'Yiminghe', label: 'yiminghe'},
-                                    {value: 'Yiminghe', label: 'yiminghe'},
-                                    {value: 'disabled', label: 'Disabled', disabled: true},
-                                ]}
-                            />
+                                value={building_id}
+                                onChange={(e) => setdepartment_id(e)}
+                            >
+                                {
+                                    departments?.map((item, index) => (
+                                        <option value={item?.id} key={index}>{item?.department_title}</option>
+                                    ))
+                                }
+                            </Select>
 
                         </div>
                     </div>
                     <div className="tabs-lists">
                         {
-                            data?.map((item, index) => (
+                            data2?.map((item, index) => (
 
                                 <div className="tabs-list-item">
                                     <div className="tabs-img-box">
@@ -202,14 +241,14 @@ const Home = () => {
 
 
                                 topType === 1 ?
-                                    late_attendances_count + on_time_attendances_count  + absent_attendances_count
-                                :   topType === 2 ?
-                                        late_attendances_count + on_time_attendances_count  + absent_attendances_count
+                                    late_attendances_count + on_time_attendances_count + absent_attendances_count
+                                    : topType === 2 ?
+                                        late_attendances_count + on_time_attendances_count + absent_attendances_count
                                         :
                                         topType === 3 ?
-                                            late_attendances_count + on_time_attendances_count  + absent_attendances_count
+                                            late_attendances_count + on_time_attendances_count + absent_attendances_count
                                             :
-                                   "0"
+                                            "0"
                             }
                         </p>
                         <p className="smallText font-family-medium">
@@ -228,9 +267,12 @@ const Home = () => {
                                     defaultValue="female"
                                     name="radio-buttons-group"
                                 >
-                                    <FormControlLabel value="female" control={<Radio onChange={() => settopType(2)}/>} label="Приходов"/>
-                                    <FormControlLabel value="male" control={<Radio onChange={() => settopType(3)}/>} label="Уходов"/>
-                                    <FormControlLabel value="other" control={<Radio onChange={() => settopType(1)}/>} label="Все"/>
+                                    <FormControlLabel value="female" control={<Radio onChange={() => settopType(2)}/>}
+                                                      label="Приходов"/>
+                                    <FormControlLabel value="male" control={<Radio onChange={() => settopType(3)}/>}
+                                                      label="Уходов"/>
+                                    <FormControlLabel value="other" control={<Radio onChange={() => settopType(1)}/>}
+                                                      label="Все"/>
                                 </RadioGroup>
                             </FormControl>
                         </div>
@@ -244,9 +286,15 @@ const Home = () => {
                                     defaultValue="female"
                                     name="radio-buttons-group"
                                 >
-                                    <FormControlLabel value="female" control={<Radio/>} label="Сегодня"/>
-                                    <FormControlLabel value="male" control={<Radio/>} label="На этой неделе"/>
-                                    <FormControlLabel value="other" control={<Radio/>} label="Этот месяц"/>
+                                    <FormControlLabel value="female" control={<Radio
+                                        onChange={() => getStatistik1(time?.getFullYear() + "-" + Number(time?.getMonth() + 1) + "-" + Number(time?.getDate()), time?.getFullYear() + "-" + Number(time?.getMonth() + 1) + "-" + tomorrow.toDateString().slice(8, 10))}/>}
+                                                      label="Сегодня"/>
+                                    <FormControlLabel value="male" control={<Radio
+                                        onChange={() => getStatistik1(time?.getFullYear() + "-" + Number(time?.getMonth() + 1) + "-" + Number(time?.getDate()), "2023-11-19")}/>}
+                                                      label="На этой неделе"/>
+                                    <FormControlLabel value="other" control={<Radio
+                                        onChange={() => getStatistik1(time?.getFullYear() + "-" + Number(time?.getMonth() + 1) + "-" + Number(time?.getDate()), "2023-11-30")}/>}
+                                                      label="Этот месяц"/>
                                 </RadioGroup>
                             </FormControl>
                         </div>
@@ -256,9 +304,15 @@ const Home = () => {
                 <div className="my-chart">
                     <div className="my-chart-title">
                         <h3 className="font-family-medium">Инфограмма по посещаемости</h3>
-                        <Calendar/>
+
+
+                        <RangePicker
+                            defaultValue={[dayjs('2023-01-01', dateFormat), dayjs('2023-01-01', dateFormat)]}
+                            onChange={onChange}
+                            format={dateFormat}
+                        />
                     </div>
-                    <PieChart series={[{ data, innerRadius: 80 }]} {...size}>
+                    <PieChart series={[{data, innerRadius: 80}]} {...size}>
                         <PieCenterLabel></PieCenterLabel>
                     </PieChart>
                 </div>
