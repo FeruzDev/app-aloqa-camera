9bd15b6b57b33ee0b65bf7715aef18e7ae429f30
shokhrukh rakhmatov <feruzjalilov7314@gmail.com>
2023-11-24 11:57:39 +0500

    with env

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import 'react-toastify/dist/ReactToastify.css';
 import Landing from "./pages/Landing";
 import PrivateRoute from "./components/PrivateRoute";
 function App() {
-
+ 
   return (
       <>
 
--- src/components/const.js
+++ src/components/const.js
@@ -1,4 +1,7 @@
 // export const API_PATH = "http://43.202.168.206/"
 // export const API_PATH = "http://43.202.168.206:8008/"
-export const API_PATH = "https://api.cradle-vision.com/"
+export const API_PATH = process.env.REACT_APP_API_URL
+// export const API_PATH = "https://api.cradle-vision.com/"
 export const CONFIG = {headers: {"Authorization": "Bearer " + localStorage.getItem('token')}}
+const apiUrl = process.env.REACT_APP_API_URL;
+const environment = process.env.REACT_APP_ENV;
